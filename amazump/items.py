b6adf99a9ec6694a93d9327fb45a870d4bce5bed
Yuvi Masory <ymasory@gmail.com>
2013-02-28 20:30:00 -0800

    [script commit]

--- amazump/items.py
+++ amazump/items.py
@@ -1,11 +1,23 @@
-# Define here the models for your scraped items
-#
-# See documentation in:
-# http://doc.scrapy.org/topics/items.html
-
 from scrapy.item import Item, Field
 
 class AmazumpItem(Item):
-    # define the fields for your item here like:
-    # name = Field()
-    pass
+    order_id = Field()
+    order_placed = Field()
+    order_total = Field()
+    shippings = Field()
+
+    # item_id = Field()
+    # item_name = Field()
+    # item_price = Field()
+    # item_quantity = Field()
+    # item_condition = Field()
+    # sales_tax = Field()
+    # payment_method = Field()
+    # purchase_date = Field()
+    # ship_date = Field()
+    # recipient = Field()
+    # shipping_address = Field()
+    # shipping_speed = Field()
+    # sold_by = Field()
+
+
